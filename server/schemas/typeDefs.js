b0db4f729b15e578ab79e06d8dd12d76a507caa0
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-06-12 11:58:24 -0500

    server.js file is laid out but not working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,23 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    password: String!
+  }
 
+  type Query {
+    getUser(id: ID!): User!
+    getUsers: [User!]!
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  type Mutation {
+    createUser(name: String!, email: String!, password: String!): User!
+    updateUser(id: ID!, name: String!, email: String!): User!
+    deleteUser(id: ID!): User!
+  }
 `;
 
 module.exports = typeDefs;
