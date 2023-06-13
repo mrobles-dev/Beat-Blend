7f5205be973c3a05110321b40d63c4aeaf6d0f44
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-06-12 20:11:33 -0500

    finalized HomePage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import React, {useState} from 'react'
 import Landing from "./components/LandingPage/LandingPage";
-import Feed from './components/Feed/Feed'
+import HomePage from './components/HomePage/HomePage'
+
 
 function App() {
-  // return <Landing />;
-  return <Feed />;
+  const [page, setPage] = useState('Feed');
+
+  const handlePageChange = (pageName) => {
+    setPage(pageName);
+  }
+
+  return <HomePage page={page} handlePageChange={handlePageChange}/>;
 }
 
 export default App;
---
+++ client/src/components/HomePage/HomePage.js
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import HeaderNav from "../Header/HeaderNav";
+import "./homePage.css";
+import Feed from "./Feed/Feed";
+import pfp from "../photos/pfp placeholder.png";
+
+const HomePage = ({ page, handlePageChange }) => {
+  // <img className='imgNav' src={pfpNav}></img>
+  const [length, setLength] = useState(0);
+  function countLength() {
+    const textarea = document.getElementById("textInput");
+    var string = textarea.value;
+    setLength(string.length);
+  }
+
+  return (
+    <>
+      <HeaderNav />
+      <div className="mainHomeContainer">
+        <div className="leftContainer">
+          {" "}
+          {/* navigation */}
+          <div className="navHeader">Navigation</div>
+          <ul>
+            <li className="listEle">
+              <div
+                onClick={() => handlePageChange("Feed")}
+                className={
+                  page === "Feed" ? "navContent activeNav" : "navContent"
+                }
+              >
+                <div>Feed</div>
+              </div>
+            </li>
+            <li className="listEle">
+              <div
+                onClick={() => handlePageChange("Profile")}
+                className={
+                  page === "Profile" ? "navContent activeNav" : "navContent"
+                }
+              >
+                <div>Profile</div>
+              </div>
+            </li>
+            <li className="listEle">
+              <div
+                onClick={() => handlePageChange("Recordings")}
+                className={
+                  page === "Recordings" ? "navContent activeNav" : "navContent"
+                }
+              >
+                <div>Recordings</div>
+              </div>
+            </li>
+            <li className="listEle">
+              <div
+                onClick={() => handlePageChange("Favorites")}
+                className={
+                  page === "Favorites" ? "navContent activeNav" : "navContent"
+                }
+              >
+                <div>Favorites</div>
+              </div>
+            </li>
+            <li className="listEle">
+              <div
+                onClick={() => handlePageChange("Notifications")}
+                className={
+                  page === "Notifications"
+                    ? "navContent activeNav"
+                    : "navContent"
+                }
+              >
+                <div>Notifications</div>
+              </div>
+            </li>
+            <li className="listEle">
+              <div
+                onClick={() => handlePageChange("Search")}
+                className={
+                  page === "Search" ? "navContent activeNav" : "navContent"
+                }
+              >
+                <div>Search</div>
+              </div>
+            </li>
+            <li className="listEle">
+              <div
+                onClick={() => handlePageChange("Settings")}
+                className={
+                  page === "Settings" ? "navContent activeNav" : "navContent"
+                }
+              >
+                <div>Settings</div>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="middleContainer">
+          {" "}
+          {/* changing */}
+          <Feed />
+        </div>
+        <div className="rightContainer">
+          <div className="newPostHeader">New Post</div>
+          <div className="formContainer">
+            <div className="topBar">
+              <div className="userInfo">
+                <img className="pfp" src={pfp} alt="profile"></img>
+                <div className="username">@you</div>
+              </div>
+            </div>
+            <form className="form">
+              <p>
+                <span className={length > 200 ? "red" : "green"}>{length}</span>
+                /200
+              </p>
+              <textarea
+                id="textInput"
+                placeholder="Type your message here... (200 char max)"
+                onChange={countLength}
+                rows='4'
+              />
+            </form>
+            <div className="uploadContainer">
+              <div className="upload postBtn">Upload Audio</div>
+              <div className="upload postBtn">Monetize?</div>
+            </div>
+            <div className="post bigPostBtn">Post!</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
